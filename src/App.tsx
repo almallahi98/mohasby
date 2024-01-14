5857b2d936639ce98b1e8fefe4eec98d3c130ca6
mohammed al mallahi <x52-@hotmail.com>
2024-01-15 01:09:02 +0300

    mohasby keik

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mohasby from './res/mohasby.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center align-middle h-full w-screen text-center text-4xl">
+      <div className='relative w-1/2 h-1/6'>
+        <img
+          className=''
+          src={mohasby} alt='mohasby logo' />
+        <div className='absolute bottom-20 right-0 left-0 space-y-10'>
+          <p>انتظروا محاسبي بحله الجديدة</p>
+          <div className='flex justify-center gap-2'>
+            <button onClick={() => window.open('https://wa.me/00966531342444', '_blank')}
+              className='bg-[#1E3778] py-2 px-6 rounded text-[#ffffff]'
+            >الاشتراك في الخدمة</button>
+
+            <button onClick={() => window.open('https://portal.mohasby.com', '_blank')}
+              className='bg-[#53c295] py-2 px-6 rounded text-[#ffffff]'
+            >دخول العملاء</button>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
