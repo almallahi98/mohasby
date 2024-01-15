5c4fc306f9bfe82a49357a78d56329e963cbda35
mohammed al mallahi <x52-@hotmail.com>
2024-01-15 15:31:56 +0300

    adding mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import mohasby from './res/mohasby.png';
 
 function App() {
   return (
-    <div className="flex justify-center items-center align-middle h-full w-screen text-center text-4xl">
-      <div className='relative w-1/2 h-1/6'>
+    <div className="flex justify-center items-center align-middle h-full w-screen text-center text-2xl md:text-4xl">
+      <div>
+      <div className='flex justify-center'>
         <img
-          className=''
+          className='h-auto md:w-1/3' 
           src={mohasby} alt='mohasby logo' />
-        <div className='absolute bottom-20 right-0 left-0 space-y-10'>
+        </div>
+        <div className=' space-y-10'>
           <p>انتظروا محاسبي بحله الجديدة</p>
-          <div className='flex justify-center gap-2'>
+          <div className='flex justify-center gap-2 px-2'>
             <button onClick={() => window.open('https://wa.me/00966531342444', '_blank')}
               className='bg-[#1E3778] py-2 px-6 rounded text-[#ffffff]'
             >الاشتراك في الخدمة</button>
@@ -20,8 +22,6 @@ function App() {
             >دخول العملاء</button>
           </div>
         </div>
-
-
       </div>
     </div>
   );
